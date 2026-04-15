698b4ccedb2f897faa2638477812f0ac5b782461
AvveGbm <143212657+AvveGbm@users.noreply.github.com>
2026-04-15 15:42:29 +0200

    added bullet script #16

---
+++ src/bullet.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+var speed: float = 400.0
+var direction: Vector2 = Vector2.RIGHT
+
+func _process(delta: float) -> void:
+		position += direction * speed * delta
+
+func _on_body_entered(body: Node2D) -> void:
+	queue_free() # Removes bullet when it hits something
+	
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+		queue_free() # Removes bullet when exiting the screen
