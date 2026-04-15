2089137d3ef7b9ce1da9042d8ffedbf9926b84ef
AvveGbm <143212657+AvveGbm@users.noreply.github.com>
2026-04-15 19:40:54 +0200

    Improved camera #4

--- src/player.gd
+++ src/player.gd
@@ -1,23 +1,27 @@
 extends CharacterBody2D
 
 @export_group("Movement Settings")
-@export var friction: float = 0.15
-@export var gravity: float = 980.0
+@export var ground_friction: float = 5.0
+@export var air_drag: float = 1
+@export var gravity: float = 500.0
+@export var max_speed: float = 800.0
 
 @onready var weapon_slot = $WeaponSlot
 @onready var weapon: Weapon = $WeaponSlot/Weapon
 
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	if not is_on_floor():
-		velocity.y += gravity * _delta
+		velocity.y += gravity * delta
+		velocity.y = min(velocity.y, 1500)
 	
 	var mouse_pos = get_global_mouse_position()
 	weapon_slot.look_at(mouse_pos)
 	
 	if Input.is_action_just_pressed("ui_accept") or Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 		fire_weapon()
-	if is_on_floor():
-		velocity.x = lerp(velocity.x, 0.0, friction)
+		
+	var current_drag = ground_friction if is_on_floor() else air_drag
+	velocity.x = move_toward(velocity.x, 0, current_drag * 100 * delta)
 	
 	move_and_slide()
 	
