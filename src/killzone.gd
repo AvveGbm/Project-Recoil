d515da80aea192061309db51365a2a4c3895f09b
sighbot <tobiaswicklander@gmail.com>
2026-04-15 20:35:03 +0200

    commit unstaged files

--- src/killzone.gd
+++ src/killzone.gd
@@ -1,7 +1,14 @@
 extends Area2D
 
+@onready var gameState: Node = %gameState
+@onready var timer: Timer = $Timer
+
 # Kill viable objects on collision
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		body.queue_free()
+		timer.start()
 		print("Game Over: Player hit the killzone!")
+
+func _on_timer_timeout():
+		gameState.resetLevel()
+	
