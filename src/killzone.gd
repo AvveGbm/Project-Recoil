408774662c45aaeaa49c58df67201ec94eee0593
sighbot <tobiaswicklander@gmail.com>
2026-04-19 13:38:36 +0200

    Add functions for seamlessly reloading the current level
    Seems to work decently, after solving conflict with player_can_move and killzone
    respawn conditions (multiple timers running in parallell caused problems).
    Added function for resetting player values and position depending on the loaded
    level.
    Loading level relies on level scene path, currently hardcoded. May not be a problem,
    depending on how level switching is implemented.
    
    Resolves #40

--- src/killzone.gd
+++ src/killzone.gd
@@ -5,9 +5,11 @@ extends Area2D
 # Kill viable objects on collision
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
+		GameState.stationary_timer_running = true # Avoids double resets
 		timer.start()
 		print("Game Over: Player hit the killzone!")
 
 func _on_timer_timeout():
 		GameState.reset_level()
+		GameState.stationary_timer_running = false
 	
