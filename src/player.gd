05caf18043aa65fcefa1829dc4cc6cf3129e7523
AvveGbm <143212657+AvveGbm@users.noreply.github.com>
2026-04-23 23:36:38 +0200

    add first real level

--- src/player.gd
+++ src/player.gd
@@ -7,14 +7,20 @@ class_name Player
 @export var gravity: float = 500.0
 @export var max_speed: float = 300.0
 
+@export_group("Fire Buffer")
+@export var fire_buffer_time: float = 0.12
+
 @onready var weapon_slot = $WeaponSlot
 @onready var weapon: Weapon = $WeaponSlot/Weapon
+
+var fire_buffer_timer: float = 0.0
 
 ## Resets player fields to their defaults
 func reset_for_level(spawn_position: Vector2) -> void:
 	weapon.refill_full()
 	global_position = spawn_position
 	velocity = Vector2.ZERO
+	fire_buffer_timer = 0.0
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
@@ -24,18 +30,28 @@ func _physics_process(delta: float) -> void:
 	var mouse_pos = get_global_mouse_position()
 	weapon_slot.look_at(mouse_pos)
 	
-	if Input.is_action_just_pressed("ui_accept") or Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		fire_weapon()
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		fire_buffer_timer = fire_buffer_time
+
+	if fire_buffer_timer > 0.0:
+		fire_buffer_timer -= delta
+		if try_fire_weapon():
+			fire_buffer_timer = 0.0
 		
 	var current_drag = ground_friction if is_on_floor() else air_drag
 	velocity.x = move_toward(velocity.x, 0, current_drag * 100 * delta)
 	
 	move_and_slide()
 	
-func fire_weapon() -> void:
-	if weapon:
-		var recoil_force = weapon.shoot()
-		if recoil_force > 0:
-			var look_direction = (get_global_mouse_position() - global_position).normalized()
-			velocity += -look_direction * recoil_force
-			velocity = velocity.limit_length(max_speed)
+func try_fire_weapon() -> bool:
+	if not weapon:
+		return false
+
+	var recoil_force = weapon.shoot()
+	if recoil_force <= 0:
+		return false
+
+	var look_direction = (get_global_mouse_position() - global_position).normalized()
+	velocity += -look_direction * recoil_force
+	velocity = velocity.limit_length(max_speed)
+	return true
