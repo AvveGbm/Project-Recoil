a8b815c479b2cda1d46f9abfc017ebdac4e14a1d
sighbot <tobiaswicklander@gmail.com>
2026-05-18 11:35:02 +0200

    feat: add score tally and level tracker to pause menu
    Uses hitherto unused player_score to display the player's
    total score, updated on level clear and kept between levels
    (unlike level_score which is reset with the level)
    
    Resolves: #60

--- src/pause_menu.gd
+++ src/pause_menu.gd
@@ -1,11 +1,15 @@
 extends Control
 
+@export var level_label: Label
+@export var total_score_label: Label
+
 func _ready() -> void:
 	visible = false
 	
 	process_mode = Node.PROCESS_MODE_WHEN_PAUSED
 	
 func open() -> void:
+	_update_pause_screen()
 	visible = true
 	get_tree().paused = true
 	
@@ -19,3 +23,7 @@ func _on_resume_button_pressed() -> void:
 func _on_main_menu_button_pressed() -> void:
 	get_tree().paused = false
 	get_tree().reload_current_scene()
+	
+func _update_pause_screen() -> void:
+	level_label.text = "LEVEL " + str(GameState.current_level_index + 1)
+	total_score_label.text = "TOTAL SCORE: " + str(GameState.player_score)
