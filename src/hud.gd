6c6ea0437ab9e13d2b502b47baf1189edb8e27f7
sighbot <tobiaswicklander@gmail.com>
2026-05-18 12:43:01 +0200

    feat: add level timer to UI
    Timer counts using time between frames for greater precision than
    a Timer node, used to display elapsed time to the UI to the hundredth
    of a second.
    
    Added has_moved boolean to player to that updated to true if the player
    has moved under its own power since level start/reset. Used to start
    the timer.
    
    Resolves: #54

--- src/hud.gd
+++ src/hud.gd
@@ -1,16 +1,32 @@
 extends Control
 
-
 @export var score_label: Label
 @export var ammo_label: Label
 
-# Called when the node enters the scene tree for the first time.
+@export var timer_label: Label
+var time_elapsed: float = 0.0
+
 func _ready() -> void:
 	await GameState.game_loaded
 	_initial_update()
 	GameState.player.weapon.ammo_changed.connect(_on_ammo_changed)
 	GameState.level_score_changed.connect(_on_score_changed)
+
+# Timer using _process for millisecond precision.
+func _process(delta: float) -> void:
+	if !GameState.player.has_moved:
+		reset_time_elapsed()
+	else:
+		time_elapsed += delta
+		
+	var minutes: int = floori(time_elapsed / 60.0)
+	var seconds: int = floori(time_elapsed) % 60
+	var hundredths: int = floori(fmod(time_elapsed, 1) * 100)
 	
+	var timer_text_formatted = "%02d:%02d.%02d" % [minutes, seconds, hundredths]
+	
+	timer_label.text = timer_text_formatted
+
 func _on_ammo_changed(current, total: int):
 	update_ammo_label(current, total)
 
@@ -26,4 +42,7 @@ func update_score_label(score: int):
 
 func update_ammo_label(amount, total: int):
 	ammo_label.text = "AMMO: " + str(amount) + "/" + str(total)
+	
+func reset_time_elapsed() -> void:
+	time_elapsed = 0.0
 	
